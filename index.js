fe47f752589994a6628d5f20d900ffab038e7627
Farzad Afshari <far.afshari@gmail.com>
2022-09-15 15:19:07 -0400

    ApplyCycles with Mienna's logic

--- index.js
+++ index.js
@@ -1,23 +1,10 @@
-import { Client, GatewayIntentBits } from 'discord.js'
 import { config as dotenvConfig } from 'dotenv'
 
-import { deez } from './commands/ping.js'
+import ApplyCycles from './cycles.js'
 
+/* Dont worry about this lol */
 dotenvConfig()
 
-const client = new Client({
-    intents: [
-        GatewayIntentBits.DirectMessages,
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent,
-    ]
-})
 
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`)
-})
-
-deez(client)
-
-client.login(process.env.CLIENT_TOKEN)
+const items = await ApplyCycles()
+console.log(items)
